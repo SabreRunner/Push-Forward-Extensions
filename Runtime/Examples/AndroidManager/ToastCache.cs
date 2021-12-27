5f96071439e1fa44fc0e18a252cc1a9226400696
Eran Arbel <sabrerunner@gmail.com>
2021-12-27 17:41:26 +0200

    Documentation

--- Runtime/Examples/AndroidManager/ToastCache.cs
+++ Runtime/Examples/AndroidManager/ToastCache.cs
@@ -1,12 +1,15 @@
-﻿
+
 namespace PushForward.Examples
 {
     using PushForward;
 
+    /// <summary>An example component used to send the given string value as an Android Toast.</summary>
     public class ToastCache : BaseMonoBehaviour
     {
+        /// <summary>The string to send as Toast.</summary>
         public string StringValue { get; set; }
 
+        /// <summary>An editor exposed method that sends the set string value as an Android Toast.</summary>
         public void SendToast()
         {
             #if UNITY_ANDROID
